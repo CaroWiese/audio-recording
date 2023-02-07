41ff8a8ff5e829d009da011f8a8439f287520f3c
CaroWiese <wiese.caro@gmail.com>
2023-02-07 12:24:01 +0100

    404 and MIME type error corrected

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 // This node project is working with an express framework. 
-// Additionally, I use dotenv to manage environnement variables,
+// Additionally, I use dotenv and cors modules to manage environnement variables
+// and cross-origin ressource sharing, respectively,
 // ejs as html templating engine and pg to interface with a PostgreSQL database.
 
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 8020;
 const router = require('./app/router');
@@ -16,6 +18,7 @@ app.set('views', __dirname + '/app/views');
 // Static route:
 app.use(express.static('public'));
 
+app.use(cors());
 // Data is sent in json format.
 app.use(express.json());
 
